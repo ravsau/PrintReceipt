1641e57bd1135fcf9e46841b2c4068d4d0cf424b
Saurav Sharma <sauravsharma011@gmail.com>
2017-05-24 16:33:08 -0400

    added the receipt

---
+++ src/PrintReceipt.java
@@ -0,0 +1,51 @@
+
+public class PrintReceipt {
+
+	public static void main(String[] args) {
+	
+	
+		System.out.print('+');
+		for (int i=1; i<40;i++){
+			System.out.print('-');
+		}
+		System.out.println('+');
+		
+		System.out.println("| \tJava Bank ATM Receipt  \t\t|");
+		System.out.println("| \tWednesday, December 2, 2015  \t|");
+		System.out.println("| \tATM Location # 123  \t\t|");
+		
+		
+		System.out.println("| \t\t\t  \t\t|");
+		System.out.println("| \t\t\t \t\t|");
+		System.out.println("| \t\t\t \t\t|");
+		
+		System.out.println("| \tAccount Number: 1234567  \t|");
+		System.out.println("| \tCustomer: John Q. Public \t|");
+		System.out.println("| \tTransaction Type: Deposit   \t|");
+		System.out.println("| \tTransaction Amount: $500.00  \t|");
+		System.out.println("| \tAccount Balance: $1,500.00  \t|");
+		
+		System.out.println("| \t\t\t \t\t|");
+		
+		System.out.println("| \tThank you for banking with us  \t|");
+		System.out.println("| \t     Have a coffee day!  \t|");
+		
+		System.out.println("| \t\t\t \t\t|");
+		System.out.println("| \t\t\t \t\t|");
+		
+		System.out.print('+');
+		for (int i=1; i<40;i++){
+			System.out.print('-');
+		}
+		System.out.println('+');
+		
+		
+	
+		
+		
+		 
+		
+		
+
+}
+}
